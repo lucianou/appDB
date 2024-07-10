a4d3929908d81a77b998e4e3f7571fa0ee7ad315
lucianouu <luciano.espinoza@alumnos.uach.cl>
2024-07-10 19:33:46 -0400

    progreso2

--- src/routes/citas.js
+++ src/routes/citas.js
@@ -6,37 +6,39 @@ const router = Router();
 // Función para obtener las comunas y los clientes de la base de datos
 const getDatos = async () => {
   try {
-    const comunaResult = await pool.query('SELECT id_comuna, nombre_comuna FROM comuna;');
-    const empleadoResult = await pool.query('SELECT id_empleado FROM empleado;');
-    const cargoResult = await pool.query('SELECT id_cargo, nombre_cargo FROM cargo;');
+    const citasResult = await pool.query('SELECT id_cita FROM cita;');
+    const empleadoResult = await pool.query('SELECT id_empleado, nombre_empleado FROM empleado;');
+    const salonResult = await pool.query('SELECT id_salon, nombre_salon FROM salon;');
+    const clienteSalonResult = await pool.query('SELECT id_cliente_salon, ref_id_cliente FROM cliente_salon;');
     return {
-      comunas: comunaResult.rows,
+      citas: citasResult.rows,
       empleados: empleadoResult.rows,
-      cargos: cargoResult.rows
+      salones: salonResult.rows,
+      clientes: clienteSalonResult.rows
     };
   } catch (error) {
     console.error('Error al obtener las comunas y empleados:', error);
-    return { comunas: [], empleado: [] , cargos: []};
+    return { citas: [], empleado: [] , salones: [], clientes: []};
   }
 };
 
 // Ruta para la página de registro de clientes (GET)
 router.get('/', async (req, res) => {
-  const { comunas, empleados, cargos } = await getDatos();
-  res.render('citas', { comunas, empleados , cargos}); // Renderizar el archivo de vista clientes.ejs con la lista de comunas
+  const { citas, empleados, clientes, salones } = await getDatos();
+  res.render('citas', { citas, empleados , clientes , salones}); // Renderizar el archivo de vista clientes.ejs con la lista de comunas
 });
 
 // Ruta para manejar el registro del cliente (POST)
 router.post('/', async (req, res) => {
-  const { rut, nombre, telefono, comunaSeleccionada, cargoSeleccionado } = req.body;
-  const { empleados } = await getDatos();
-  const nuevoIdEmpleado = empleados.length + 1;
+  const { estadoCita, horaInicio, horaFin, empleadoSeleccionado, clienteSalonSeleccionado, salonSeleccionado } = req.body;
+  const { citas } = await getDatos();
+  const nuevoIdCita = citas.length + 1;
 
   try {
     // Inserción en la base de datos
-    await pool.query('INSERT INTO cita (id_cita, estado_cita, horario_ini,horario_fin, ref_id_empleado,ref_id_cliente_salon,ref_id_salon) VALUES ($1, $2, $3, $4)', 
-      [nuevoIdCita ,estadoCita, horaInicio, horaFin, peluqueroSeleccionado, clienteSeleccinado, salonSeleccionado]);
-    res.send(`Cita agendada para el cliente: ${nombreCliente}, RUT: ${rutCliente}, con el peluquero: ${peluqueroSeleccionado}, en la fecha: ${fechaSeleccionada} a las: ${horaSeleccionada}`);
+    await pool.query('INSERT INTO cita (id_cita, estado_cita, horario_ini,horario_fin, ref_id_empleado,ref_id_cliente_salon,ref_id_salon) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+      [nuevoIdCita ,estadoCita, horaInicio, horaFin, empleadoSeleccionado, clienteSalonSeleccionado, salonSeleccionado]);
+    res.send(`Cita agendada para el cliente: ${clienteSalonSeleccionado}, salon: ${salonSeleccionado}, con el peluquero: ${empleadoSeleccionado}, a las: ${horaInicio}`);
   } catch (error) {
     console.error('Error al agendar la cita:', error);
     res.status(500).send('Error al agendar la cita');
