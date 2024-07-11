5c73ee8c679ac7a098442cd21dfb2f27053146a6
lucianouu <luciano.espinoza@alumnos.uach.cl>
2024-07-11 14:23:30 -0400

    11076

--- src/routes/citas.js
+++ src/routes/citas.js
@@ -36,7 +36,7 @@ router.post('/', async (req, res) => {
   try {
     // Inserción en la base de datos
     await pool.query('INSERT INTO cita (id_cita, estado_cita, dia, horario_ini, horario_fin, ref_id_empleado, ref_id_cliente_salon, ref_id_salon) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
-      [nuevoIdCita, estadoCita || 0, horaInicio, horaFin, fecha, empleadoSeleccionado, clienteSalonSeleccionado, salonSeleccionado]);
+      [nuevoIdCita, estadoCita || 0, fecha, horaInicio, horaFin, empleadoSeleccionado, clienteSalonSeleccionado, salonSeleccionado]);
     res.redirect('/citas');
   } catch (error) {
     console.error('Error al agendar la cita:', error);
